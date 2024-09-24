ff4a9589ac2c660a67935d3dc583c17fe2596b3c
Dineshmoorthi27 <dineshmoorthi27.gmail.com>
2024-09-25 02:32:09 +0530

    update: javascript file

--- Asserts/js/portfolio_js.js
+++ Asserts/js/portfolio_js.js
@@ -3,3 +3,18 @@ document.getElementById('menu-icon').addEventListener('click', function () {
     const navItems = document.getElementById('nav-items');
     navItems.classList.toggle('show');
 });
+//Script to download resume
+
+function downloadResume() {
+    const link = document.createElement('a');
+    link.href = '../Asserts/documents/Dinesh_Resume.pdf';
+    link.download = 'Resume.pdf';
+    link.click();
+}
+
+// script to scroll to contact section
+function scrollToContact() {
+    document.getElementById('Contact').scrollIntoView({
+        behavior: 'smooth'  // Enables smooth scrolling
+    });
+}
