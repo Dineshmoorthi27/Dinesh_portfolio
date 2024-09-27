57cc940c24243b1e3634f4a06d63f2dd88f36f73
Dineshmoorthi27 <dineshmoorthi27.gmail.com>
2024-09-28 01:52:33 +0530

    refactor: home page background changed

--- Asserts/js/portfolio_js.js
+++ Asserts/js/portfolio_js.js
@@ -39,3 +39,12 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("load", checkVisibility);
 });
 
+// Lottie animation setup
+var hackathonAnimation = lottie.loadAnimation({
+    container: document.getElementById('lottie-hackathon'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://lottie.host/247099a0-13c1-400f-91ec-185ff22033a4/ve8tJMYuin.json' // Replace with desired Lottie file
+});
+
