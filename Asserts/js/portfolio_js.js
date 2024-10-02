ff07c94a5e7acb1455d191eecd6bcdffea6f84f1
Dineshmoorthi27 <dineshmoorthi27.gmail.com>
2024-10-02 20:06:51 +0530

    fix: contact issue in about section

--- Asserts/js/portfolio_js.js
+++ Asserts/js/portfolio_js.js
@@ -16,47 +16,9 @@ document.querySelectorAll('.nav-items a').forEach(link => {
 });
 
 //Script to download resume
-
 function downloadResume() {
     const link = document.createElement('a');
     link.href = 'Asserts/documents/Dinesh_Resume.pdf';
     link.download = 'Resume.pdf';
     link.click();
 }
-
-// script to scroll to contact section
-function scrollToContact() {
-    document.getElementById('Contact').scrollIntoView({
-        behavior: 'smooth'  // Enables smooth scrolling
-    });
-}
-
-// script to check the visibility of skill section
-document.addEventListener("DOMContentLoaded", function () {
-    const progressBars = document.querySelectorAll(".progress-bar");
-
-    function checkVisibility() {
-        progressBars.forEach(function (bar) {
-            const barTop = bar.getBoundingClientRect().top;
-            const screenBottom = window.innerHeight;
-
-            if (barTop < screenBottom) {
-                const skillLevel = bar.getAttribute("data-skill");
-                bar.style.width = skillLevel + "%";
-            }
-        });
-    }
-
-    window.addEventListener("scroll", checkVisibility);
-    window.addEventListener("load", checkVisibility);
-});
-
-// Lottie animation setup
-var hackathonAnimation = lottie.loadAnimation({
-    container: document.getElementById('lottie-hackathon'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'https://lottie.host/247099a0-13c1-400f-91ec-185ff22033a4/ve8tJMYuin.json' // Replace with desired Lottie file
-});
-
